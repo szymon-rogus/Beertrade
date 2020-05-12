9ae3044617981c582bcaf3164ea3fc5647852e9a
szymon-rogus <sszymek98@gmail.com>
2020-05-12 22:39:55 +0200

    #10: Beer model v1.0

---
+++ src/main/java/pl/beertrade/model/beer/Beer.java
@@ -0,0 +1,47 @@
+package pl.beertrade.model.beer;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Entity
+@Table(name = "BEER")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+public class Beer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false)
+    private UUID id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private String type; //TODO create some enum for type
+
+    @NotNull
+    private float alcoholPercentage;
+
+    @NotNull
+    private String brand; //TODO also some enum would be nice
+
+    private String color;
+
+    private String description;
+
+    @NotNull
+    private boolean onStore;
+
+    @NotNull
+    private boolean onSystem;
+
+}
---
+++ src/main/java/pl/beertrade/model/beer/BoughtBeer.java
@@ -0,0 +1,32 @@
+package pl.beertrade.model.beer;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.UUID;
+
+@Entity
+@Table(name = "BOUGHT_BEER")
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+public class BoughtBeer {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(updatable = false)
+    private UUID id;
+
+    @NotNull
+    @OneToOne
+    @JoinColumn(name = "BEER_ID", referencedColumnName = "ID")
+    private Beer beer;
+
+    @NotNull
+    private float price;
+}
