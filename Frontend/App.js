70d64df695479345b715887f7962615835674818
Bartłomiej Łagosz <lagosz.bartlomiej@gmail.com>
2020-10-19 17:55:17 +0100

    #40 Implement bartender view and distinct between bartender and client role while logging in

--- Frontend/App.js
+++ Frontend/App.js
@@ -6,17 +6,20 @@ import ForgottenPasswordPage from './src/ForgottenPassword/ForgottenPasswordPage
 import PricesPage from "./src/Prices/PricesPage";
 import { NavigationContainer } from '@react-navigation/native';
 import stack from './Global.js'
+import BartenderOrderPage from './src/BartenderOrderPage/BartenderOrderPage.js';
 
 export default class App extends React.Component {
   render() {
     return (
       <NavigationContainer>
         <stack.Navigator>
-          <stack.Screen name='login' component={ LoginPage } options={ { headerShown: false } } />
-          <stack.Screen name='registration' component={ RegistrationPage } options={ { headerShown: false } } />
-          <stack.Screen name='forgottenpass' component={ ForgottenPasswordPage } options={ { headerShown: false } } />
-          <stack.Screen name='prices' component={PricesPage} options={{headerShown: false}}/>
+          <stack.Screen name='login' component={LoginPage} options={{ headerShown: false }} />
+          <stack.Screen name='registration' component={RegistrationPage} options={{ headerShown: false }} />
+          <stack.Screen name='forgottenpass' component={ForgottenPasswordPage} options={{ headerShown: false }} />
+          <stack.Screen name='prices' component={PricesPage} options={{ headerShown: false }} />
+          <stack.Screen name='bartenderOrder' component={BartenderOrderPage} options={{ headerShown: false }} />
         </stack.Navigator>
       </NavigationContainer>
-  );
+    );
+  }
 }
---
+++ Frontend/src/BartenderOrderPage/BartenderOrderPage.js
@@ -0,0 +1,82 @@
+import React, { Component }  from 'react';
+import { http } from '../../Global.js'
+import { View, Text, FlatList, TouchableOpacity } from "react-native";
+import { globalStyles } from '../../GlobalStyles.js'
+import { styles } from './BartenderOrderPageStyles.js'
+ 
+const OrderItem = ({ item, onPressFunc }) => (
+      <TouchableOpacity onPress={ () => onPressFunc(item.id) } style={ styles.item }>
+        <Text style={ styles.itemText }>Name: { item.name }</Text>
+        <Text style={ styles.itemText }>User: { item.userLogin }</Text>
+      </TouchableOpacity>
+)
+
+export default class BartenderOrderPage extends Component {
+    
+    state = {
+      items: []
+    }
+
+    constructor(props) {
+      super(props);
+      this.updateItems();
+    }
+
+    updateItems = async () => {
+      http.get('/product/orders').then((response) => {
+        fetchedList = [];
+        for (let order of response.data) {
+          fetchedList.push(order);
+      }
+      this.setState({
+        items: fetchedList
+      });
+      }).catch(err => console.log(err));
+    }
+
+    sendRequestDeleteItem = (id) => {
+      actualItems = this.state.items;
+      modifiedList = actualItems.filter(item => item.id !== id);
+      this.setState({ items: modifiedList });
+      http.delete('/order/' + id).catch((err) => {
+        console.loeg(err);
+      });
+
+    }
+
+    renderItem = ({item}) => {
+      return (
+          <OrderItem item={ item } onPressFunc = { this.sendRequestDeleteItem }/>
+      );
+  };
+
+  updateInterval = setInterval(async () => {
+    this.updateItems();
+  }, 5000)
+
+    componentDidMount() {
+      try {
+        this.updateInterval;
+      } catch(e) {
+        console.log(e);
+      }
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.updateInterval);
+    }
+
+    render() {
+        return (
+          <View style={ globalStyles.mainContainer }>
+            <Text style={ globalStyles.title }>Order List</Text>
+            <FlatList
+                data={this.state.items}
+                renderItem={this.renderItem}
+                keyExtractor={(item) => item.id}
+            />
+          </View>
+        )
+    }
+
+}
