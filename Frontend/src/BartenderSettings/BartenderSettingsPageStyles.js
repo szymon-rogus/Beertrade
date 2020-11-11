26414da956b3c975e4e0b783d4e3a5b6a5fa5ef9
Bartłomiej Łagosz <lagosz.bartlomiej@gmail.com>
2020-11-11 18:30:46 +0100

    Bartender frontend pages tbd

---
+++ Frontend/src/BartenderSettings/BartenderSettingsPageStyles.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  title: {
+    textAlign: 'center',
+    color: 'white',
+    fontSize: 28
+  },
+  topBar: {
+    flex: 0.15,
+    backgroundColor: 'darkblue',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  titleBox: {
+    flex: 0.5,
+    margin: 15,
+    backgroundColor: 'darkblue',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  },
+  navIconsBox: {
+    flex: 0.5,
+    margin: 15,
+    backgroundColor: 'darkblue',
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
+  },
+  listTitle: {
+    fontSize: 18,
+    color: 'black',
+    marginBottom: 12
+  },
+  listImage: {
+    width: 50,
+    height: 100,
+    resizeMode: Image.resizeMode.contain
+  },
+  listAttributes: {
+    fontSize: 14,
+    color: 'black'
+  }
+});
