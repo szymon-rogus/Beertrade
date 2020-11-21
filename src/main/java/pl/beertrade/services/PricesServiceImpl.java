b150e5406ecdad7c9cf4e31909d154d12930f696
szymon-rogus <sszymek98@gmail.com>
2020-11-21 18:53:26 +0100

    removed some comments

--- src/main/java/pl/beertrade/services/PricesServiceImpl.java
+++ src/main/java/pl/beertrade/services/PricesServiceImpl.java
@@ -2,28 +2,28 @@ package pl.beertrade.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-//import pl.beertrade.services.prices.PricesModifier;
-//import pl.beertrade.services.prices.PricesService;
+import pl.beertrade.services.prices.PricesModifier;
+import pl.beertrade.services.prices.PricesService;
 
 import java.util.Map;
 import java.util.UUID;
 
 @Service
-public class PricesServiceImpl /*implements PricesService*/ {
+public class PricesServiceImpl implements PricesService {
 
-    //@Autowired
-    //private PricesModifier pricesModifier;
+    @Autowired
+    private PricesModifier pricesModifier;
 
     private Map<UUID, Double> prices;
 
-    //@Override
+    @Override
     public void countNewPrices() {
         synchronized (prices) {
 
         }
     }
 
-    //@Override
+    @Override
     public void getPrices() {
 
     }
